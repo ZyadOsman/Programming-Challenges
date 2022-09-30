bd970aec9ec19c1a288c688d3a16bdc9b07f632e
ZyadOsman <113169670+ZyadOsman@users.noreply.github.com>
2022-09-30 12:47:28 +0100

    Add files via upload

--- Challenge 4/Wind_Chill.py
+++ Challenge 4/Wind_Chill.py
@@ -1,10 +1,18 @@
-air_temp = float(input("Enter AIR TEMPERATURE: "))
-wind_spd = float(input("Enter WAVE SPEED: "))
+def airtemp():
+    air_temp = float(input("Enter AIR TEMPERATURE: "))
+    return air_temp
 
-def calculations():
-     windchill = 35.74+0.6215*air_temp-35.75*wind_spd**0.16+0.4275*air_temp*wind_spd**0.16
+def windspeed():
+    wind_spd = float(input("Enter WAVE SPEED: "))
+    return wind_spd
+
+def calculations(airtemp, windspeed):
+     windchill = 35.74+0.6215*airtemp-35.75*windspeed**0.16+0.4275*airtemp*windspeed**0.16
      return windchill
 
 if __name__ == "__main__":
-    windchill = calculations()
+    airtemp = airtemp()
+    windspeed = windspeed()
+    windchill = calculations(airtemp, windspeed)
     print (f"Windchill: {windchill}")
+    
